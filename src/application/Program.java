b1064c6ad267fb5c082861cc6a2920a3993822d6
Caio André <caioserralvo182@gmail.com>
2020-09-29 23:58:11 -0300

    Teste dois, alteração no pacote

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
-package curso;
+package application;
 
 public class Program {
 	public static void main(String[] args) {
 		System.out.println("teste");
+		
+		System.out.println("teste 2");
 	}
 }
